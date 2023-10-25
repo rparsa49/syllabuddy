c9354d2382428fb32eeab7f517b942bf627e684e
Jordan <jordanrivas@mail.adelphi.edu>
2023-10-24 23:09:17 -0400

    Made login front end :D

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import RegistrationPage from "./components/register";
 import LandingPage from "./components/landing";
 import Login from "./components/login";
 import NavBar from "./components/navbar";
+import Landing from "./components/landing";
 function App() {
   return (
     <main>
       <NavBar />
-      <LandingPage />
+      <Landing/>
+      
     </main>
   );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,23 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [pass , setPass ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  }
+ 
   return (
     <div>
-      <header>
-        <h1>Login to Syllabuddy</h1>
-      </header>
+      
       <section id ="login">
-      {
+      <div className="min-h-screen flex flex-col bg-background">
+        <div className=" flex-grow flex items-center justify-center">
+          <div className="bg-white p-8 rounded-lg shadow-md w-full sm:w-2/3 lg:w-1/2">
+          <header>
+              <h1 className="text-3xl font-semibold text-center mb-10 text-primary">Login to Syllabuddy</h1>
+          </header>
+        <>
         <form>
-         <label for ="email">email</label>
-         <input type = "email" placeholder ="email@email.edu" id ="email" name="email"> </input> 
-         <label for ="password">password</label>
-         <input type = "password" placeholder ="Password shhhh" id ="password" name="password"> </input>  
+          <div className = "mb-10">
+         <label className="block text-sm font-semibold text-text" for="email">Email:</label>
+         <input value={email} onChange={(e) => setEmail(e.target.value)} 
+         className="input input-bordered input-accent w-full max-w-xs " 
+         type="email" 
+         placeholder="email@email.edu" 
+         id="email" 
+         name="email"/> 
+         </div>
+         
+        <div className = "mb-4">
+         <label className="block text-sm font-semibold text-text" for="password">Password:</label>
+         <input value={pass} onChange={(e) => setPass(e.target.value)} 
+         className="input input-bordered input-accent w-full max-w-xs" 
+         type="password" 
+         placeholder="Password shhhh" 
+         id="password" 
+         name="password"/>  
+         </div>
 
+        <div className = "mb-4 flex justify-center ">
+         <button className=" block btn btn-secondary " type = "submit">  Log In </button>
+         </div>
          </form>
-
-
-      }</section>
+         <div className ="mb-4 flex justify-center">
+         <button className=" block btn btn-primary">  Don't have an account? Register Here </button>
+         </div>
+        </>
+      </div>
+      </div>
+      </div>
+      </section>
     </div>
   );
 };
