b492d6db6774035b115a131d24c9bd15c785d2ec
roya parsa <99053997+rparsa49@users.noreply.github.com>
2023-12-04 07:53:13 -0500

    fix for course display

--- src/components/coursedisplay.js
+++ src/components/coursedisplay.js
@@ -36,16 +36,12 @@ const CourseDisplayPage = (props) => {
             if (response.status === 200) {
                 const data = await response.json();
                 console.log(data);
+
+                // Parse the tags string into an array
+                const parsedTags = JSON.parse(data.tags).tags || [];
                 setDisplayData(data);
-                setTags(data.tags);
-                //const tagsObject = JSON.parse(tags);
-                //tagsValues = tagsObject.tags;
-                console.log(tags)
-                setTerms(data.terms);
-                //const termsObject = JSON.parse(terms);
-                //termsValues = termsObject.terms;
-                console.log(terms);
-                console.log(data);
+                setTags(parsedTags);
+                console.log(parsedTags);
             } else console.log("display data: ", displayData);
             } catch (error) {
             console.log("Error while loading display page:", error);
@@ -59,82 +55,84 @@ const CourseDisplayPage = (props) => {
 
   return (
     <div className="min-h-screen flex flex-col bg-newbg">
-        <main className="container mx-auto px-6 pt-10 flex-1 h-screen">
-            {displayData.courseCode != "DUMMY" ? (
-            <>
-                {/* Course name, code, and university name */}
-                <div className= "text-left mb-5">
-                    <h1 className="inline-headings text-3xl md:text-4xl lg:text-6xl font-sans font-bold text-newtext">
-                        {displayData.courseName}
-                    </h1>
-                    <h2 className= "inline-headings text-xl md:text-2xl lg:text-4xl font-sans text-newtext">
-                        {displayData.profName}
-                    </h2>
-                    <h3 className="text-l md:text-xl lg:text-2xl font-sans text-newtext text-left">
-                        {displayData.university} | {displayData.courseCode}
-                    </h3>
-                </div>
-                
-                {/* Course Description */}
-                <h1 className= "text-l md:text-xl lg:text-3xl pt-1 font-sans mb-4 text-newtext text-left">
-                    {displayData.courseDesc}
-                </h1>
-                
-                {/* Course Tags */}
-                <div className= "text-left">
-                    {tags((item, index) => (
-                    <tag key={index} className="badge badge-secondary mr-2">
-                        {tags[index]}
-                    </tag>
-                    ))}
-                </div>
+      <main className="container mx-auto px-6 pt-10 flex-1 h-screen">
+        {displayData.courseCode != "DUMMY" ? (
+          <>
+            {/* Course name, code, and university name */}
+            <div className="text-left mb-5">
+              <h1 className="inline-headings text-3xl md:text-4xl lg:text-6xl font-sans font-bold text-newtext">
+                {displayData.courseName}
+              </h1>
+              <h2 className="inline-headings text-xl md:text-2xl lg:text-4xl font-sans text-newtext">
+                {displayData.profName}
+              </h2>
+              <h3 className="text-l md:text-xl lg:text-2xl font-sans text-newtext text-left">
+                {displayData.university} | {displayData.courseCode}
+              </h3>
+            </div>
 
-                {/* Course Average */}
-                <div className = "text-l md:text-xl lg:text-2xl mt-6 text-newtext text-center mb-5">
-                    On average, students in this course receive
-                </div>
-                <div style={{
-                    display: 'flex',
-                    justifyContent:'center',
-                    alignItems:'center',
-                    height: '10vh'
-                }}>
-                    <div className="radial-progress text-newbg bg-newacc text-newprim-content border-4 border-newacc" 
-                        style={{"--value":displayData.averageGrade, "--thickness": "3px"}}>{displayData.averageGrade}%
-                    </div>
-                </div>
-                
+            {/* Course Description */}
+            <h1 className="text-l md:text-xl lg:text-3xl pt-1 font-sans mb-4 text-newtext text-left">
+              {displayData.courseDesc}
+            </h1>
 
-                {/* Available Syllabi */}
-                <div className="text-l md:text-xl lg:text-3xl pt-1 font-sans mb-4 text-newtext text-left">
-                    Available Syllabi:
+            {/* Course Tags */}
+            <div className="text-left">
+              {tags.map((tag, index) => (
+                <span key={index} className="badge badge-secondary mr-2">
+                  {tag}
+                </span>
+              ))}
+            </div>
+
+            {/* Course Average */}
+            <div className="text-l md:text-xl lg:text-2xl mt-6 text-newtext text-center mb-5">
+              On average, students in this course receive
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "10vh",
+              }}
+            >
+              <div
+                className="radial-progress text-newbg bg-newacc text-newprim-content border-4 border-newacc"
+                style={{
+                  "--value": displayData.averageGrade,
+                  "--thickness": "3px",
+                }}
+              >
+                {displayData.averageGrade}%
+              </div>
+            </div>
+
+            {/* Available Syllabi */}
+            <div className="text-l md:text-xl lg:text-3xl pt-1 font-sans mb-4 text-newtext text-left">
+              Available Syllabi:
+            </div>
+
+            <div>
+              {terms.map((item, index) => (
+                <div key={index} className="collapse collapse-arrow bg-newprim">
+                  <input type="radio" name="my-accordion-2" checked="checked" />
+                  <div className="collapse-title text-xl font-medium text-base-100">
+                    {terms[index]}
+                  </div>
+                  <div className="collapse-content text-base-100">
+                    <a href="TestPDFfile.pdf" download="syllabus.pdf">
+                      <button className="btn btn-warning">Download</button>
+                    </a>
+                  </div>
                 </div>
-                
-                <div>
-                    {terms.map((item, index) => (
-                        <div key={index} className="collapse collapse-arrow bg-newprim">
-                        <input type="radio" name= "my-accordion-2" checked="checked"/>
-                        <div className="collapse-title text-xl font-medium text-base-100">
-                            {terms[index]} 
-                        </div>
-                        <div className="collapse-content text-base-100">
-                            <a href="TestPDFfile.pdf" download= "syllabus.pdf">
-                                <button className="btn btn-warning">
-                                    Download
-                                </button>
-                            </a>
-                        </div>
-                        </div>
-                    ))}
-                    
-                    
-                
-                </div>
-            </>
-            ) : (
-                <p>Loading...</p>
-            )}
-        </main>
+              ))}
+            </div>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </main>
     </div>
   );
 };
